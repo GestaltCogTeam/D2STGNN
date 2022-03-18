e7306d7ecfb8233942d31cdf31562526a192b1e1
Zezhi Shao <864453277@qq.com>
2022-03-18 09:11:00 +0800

    remove unused parameter

--- models/DynamicGraphConv/Utils/mask.py
+++ models/DynamicGraphConv/Utils/mask.py
@@ -4,12 +4,10 @@ import torch.nn as nn
 class Mask(nn.Module):
     def __init__(self, **model_args):
         super().__init__()
-        self.device = model_args['device']
-        self.mask   = model_args['adjs']
     
     def _mask(self, index, adj):
         mask = self.mask[index] + torch.ones_like(self.mask[index]) * 1e-7      # TODO: 测试删掉无穷小
-        return mask * adj
+        return mask.to(adj.device) * adj
 
     def forward(self, adj):
         result = []
